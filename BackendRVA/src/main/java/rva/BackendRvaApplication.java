3592d44608bf9d5f510833c9d73a0fc52458eb40
JelenaMedareviccc <medarevicj@gmail.com>
2018-06-05 13:55:04 +0200

    Deployment

--- BackendRVA/src/main/java/rva/BackendRvaApplication.java
+++ BackendRVA/src/main/java/rva/BackendRvaApplication.java
@@ -7,10 +7,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendRvaApplication {
+public class BackendRvaApplication extends SpringBootServletInitializer {
+	
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(BackendRvaApplication.class);
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendRvaApplication.class, args);
